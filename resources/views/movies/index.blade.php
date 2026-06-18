10f71105306e6e77b12df717b20fba85b28ad098
Mutiah Nisa Putri <putrimutiahnisa@gmail.com>
2026-06-18 14:24:43 +0700

    Display movie search result

--- resources/views/movies/index.blade.php
+++ resources/views/movies/index.blade.php
@@ -4,7 +4,7 @@
 
 <h2 class="mb-4">Search Movie</h2>
 
-<form>
+<form class="mb-4">
     <div class="input-group">
         <input
             type="text"
@@ -17,5 +17,35 @@
         </button>
     </div>
 </form>
+
+<div class="row">
+
+    <div class="col-md-3 mb-4">
+        <div class="card h-100">
+
+            <img
+                src="https://via.placeholder.com/300x450"
+                class="card-img-top"
+                alt="Movie">
+
+            <div class="card-body">
+                <h5 class="card-title">
+                    Avengers Endgame
+                </h5>
+
+                <p class="card-text">
+                    2019
+                </p>
+
+                <a href="#"
+                   class="btn btn-primary btn-sm">
+                    Detail
+                </a>
+
+            </div>
+        </div>
+    </div>
+
+</div>
 
 @endsection
