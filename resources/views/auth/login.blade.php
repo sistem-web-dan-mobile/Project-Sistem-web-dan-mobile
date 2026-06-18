ebc6625895ab4c6605746f6047db852a5c19bc97
Mutiah Nisa Putri <putrimutiahnisa@gmail.com>
2026-06-18 14:16:48 +0700

    Create login page UI

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,47 @@
+@extends('layouts.app')
+
+@section('content')
+
+<div class="row justify-content-center">
+    <div class="col-md-6">
+
+        <div class="card">
+            <div class="card-header">
+                <h3>Login</h3>
+            </div>
+
+            <div class="card-body">
+
+                <form>
+
+                    <div class="mb-3">
+                        <label>Email</label>
+                        <input
+                            type="email"
+                            class="form-control"
+                            placeholder="Masukkan Email">
+                    </div>
+
+                    <div class="mb-3">
+                        <label>Password</label>
+                        <input
+                            type="password"
+                            class="form-control"
+                            placeholder="Masukkan Password">
+                    </div>
+
+                    <button
+                        type="submit"
+                        class="btn btn-primary">
+                        Login
+                    </button>
+
+                </form>
+
+            </div>
+        </div>
+
+    </div>
+</div>
+
+@endsection
