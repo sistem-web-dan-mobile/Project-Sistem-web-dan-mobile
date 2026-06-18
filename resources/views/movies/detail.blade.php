50ed8efec9a79cd15b369891fb071375b253a4e4
Mutiah Nisa Putri <putrimutiahnisa@gmail.com>
2026-06-18 14:26:00 +0700

    Create movie detail page

---
+++ resources/views/movies/detail.blade.php
@@ -0,0 +1,48 @@
+@extends('layouts.app')
+
+@section('content')
+
+<div class="row">
+
+    <div class="col-md-4">
+        <img
+            src="https://via.placeholder.com/300x450"
+            class="img-fluid">
+    </div>
+
+    <div class="col-md-8">
+
+        <h2>Avengers Endgame</h2>
+
+        <p>
+            <strong>Year:</strong> 2019
+        </p>
+
+        <p>
+            <strong>Genre:</strong>
+            Action, Adventure
+        </p>
+
+        <p>
+            <strong>Director:</strong>
+            Anthony Russo
+        </p>
+
+        <p>
+            <strong>Rating:</strong>
+            8.4
+        </p>
+
+        <p>
+            After the devastating events...
+        </p>
+
+        <button class="btn btn-success">
+            Add Favorite
+        </button>
+
+    </div>
+
+</div>
+
+@endsection
