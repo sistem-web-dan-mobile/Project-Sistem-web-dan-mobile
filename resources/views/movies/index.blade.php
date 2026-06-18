0cbff0b29a78d6387bfaacd567c742c587192138
asmarani17 <230705043@student.ar-raniry.ac.id>
2026-06-18 15:34:57 +0700

    Enhance movie search interface

--- resources/views/movies/index.blade.php
+++ resources/views/movies/index.blade.php
@@ -2,48 +2,69 @@
 
 @section('content')
 
-<h2 class="mb-4">Search Movie</h2>
+<div class="row mb-4">
 
-<form class="mb-4">
-    <div class="input-group">
-        <input
-            type="text"
-            class="form-control"
-            placeholder="Cari film...">
+    <div class="col-md-12">
 
-        <button
-            class="btn btn-primary">
-            Search
-        </button>
+        <h2 class="text-center">
+            Search Movie
+        </h2>
+
+        <p class="text-center text-muted">
+            Find your favorite movies instantly
+        </p>
+
+        <form>
+            <div class="input-group">
+
+                <input
+                    type="text"
+                    class="form-control"
+                    placeholder="Search movie title...">
+
+                <button
+                    class="btn btn-primary">
+                    Search
+                </button>
+
+            </div>
+        </form>
+
     </div>
-</form>
+
+</div>
 
 <div class="row">
 
     <div class="col-md-3 mb-4">
-        <div class="card h-100">
+
+        <div class="card h-100 shadow-sm">
 
             <img
                 src="https://via.placeholder.com/300x450"
                 class="card-img-top"
-                alt="Movie">
+                alt="Movie Poster">
 
             <div class="card-body">
+
                 <h5 class="card-title">
                     Avengers Endgame
                 </h5>
 
-                <p class="card-text">
+                <p class="card-text text-muted">
                     2019
                 </p>
 
-                <a href="#"
-                   class="btn btn-primary btn-sm">
-                    Detail
+                <a
+                    href="#"
+                    class="btn btn-primary btn-sm">
+                    View Detail
                 </a>
 
             </div>
+
         </div>
+
     </div>
 
 </div>
